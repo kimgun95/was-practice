de2985a68a3f5bf51fc30b1b0df998165c3b0db7
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2023-02-03 15:48:17 +0900

    HTTP Request 중 RequestLine test,생성

---
+++ src/test/java/RequestLineTest.java
@@ -0,0 +1,15 @@
+import org.example.RequestLine;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RequestLineTest {
+
+    @Test
+    public void create() {
+        RequestLine requestLine = new RequestLine("GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1");
+
+        assertThat(requestLine).isNotNull();
+        assertThat(requestLine).isEqualTo(new RequestLine("GET", "/calculate", "operand1=11&operator=*&operand2=55"));
+    }
+}
